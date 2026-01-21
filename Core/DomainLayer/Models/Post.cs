fca55b8e462ec60ecdd8d4deb069dfeddc88bdb8
asmaa <ae2297@fayoum.edu.eg>
2026-01-21 20:38:26 +0200

    Users-Comment-Post

---
+++ Core/DomainLayer/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ Core/DomainLayer/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Student : User
+    {
+        public string GraduationProjectDatails { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string Skills { get; set; } = null!;
+
+    }
+}
